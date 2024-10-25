f0f832310ce32c991a048a1271ccb18be95204d2
Jean-Michel Terreyre <jmt@Mac.lan>
2024-10-25 23:55:38 +0200

    first commit

---
+++ src/entities/enemyBoss.js
@@ -0,0 +1,22 @@
+
+export function makeBoss(k, initialPos) {
+  return k.make([
+    k.pos(initialPos),
+    k.sprite("burner", { anim: "idle"}),
+    k.area({ shape: new k.Rect(k.vec2(0,10), 12, 12)}),
+    k.body({mass:100, jumpForce: 320}),
+    k.anchor("center"),
+    k.state("idle", [
+        "idle",
+        "follow",
+        "open-fire",
+        "fire",
+        "shut-fire",
+        "explode",
+    ]),
+    k.health(15),
+    k.opacity(1),
+  ])
+}
+
+/* 3H25 33 */
